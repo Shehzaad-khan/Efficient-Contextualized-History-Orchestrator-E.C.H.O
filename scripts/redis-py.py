788a2c911f469a62e07c5bf916be2e8c48f566be
Shehzaad_Khan <mdshehzaadkhan001@gmail.com>
2026-03-24 08:50:50 +0530

    redis-setup

---
+++ scripts/redis-py.py
@@ -0,0 +1,15 @@
+import redis
+import os
+from dotenv import load_dotenv
+
+load_dotenv(r"C:\E.C.H.O\.env")
+
+r = redis.Redis.from_url(
+    os.environ["REDIS_URL"],
+    decode_responses=True
+
+)
+
+r.set('foo', 'bar')
+value = r.get('foo')
+print(value)  
